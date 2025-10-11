fe0e6a0211e08d70d6e20ef5d6f9e657ee681118
Corey Stock <coreystock@gmail.com>
2025-10-11 07:29:52 -0700

    telescope: ignore font files

--- init.lua
+++ init.lua
@@ -420,10 +420,9 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
+        defaults = {
+          file_ignore_patterns = { '%.woff2', '%.ttf' },
+        },
         -- },
         -- pickers = {}
         extensions = {
