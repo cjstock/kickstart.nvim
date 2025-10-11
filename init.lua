eb80febc03dce55f629e0c5a6781546428e1e418
Corey Stock <coreystock@gmail.com>
2025-10-11 07:29:14 -0700

    autocmd: configured help files to always open in vsplit by default

--- init.lua
+++ init.lua
@@ -223,6 +223,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'help' },
+  desc = 'Open help in vsplit',
+  group = vim.api.nvim_create_augroup('help', { clear = true }),
+  callback = function()
+    vim.cmd 'wincmd L'
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
